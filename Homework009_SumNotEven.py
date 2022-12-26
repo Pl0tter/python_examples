56f91c8cd700043d71c529cf66e2d30ec525c76e
Vladimir Zagovorchev <zagovorchev@yandex.ru>
2022-12-26 20:07:03 +0300

    add Homework 009 - Sum of Not Even Elements

---
+++ Homework009_SumNotEven.py
@@ -0,0 +1,28 @@
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на позиции с нечетным индексом.
+# Пример:
+# [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+def input_row():
+    while True:
+        try:
+            row_input = input(
+                "Введите список из нескольких чисел через пробел: ").split()
+            if len(row_input) < 2:
+                print("Это не список. Попробуй ещё раз")
+            else:
+                for i in range(len(row_input)):
+                    row_input[i] = int(row_input[i])
+                return row_input
+        except:
+            print("Это не целые числа. Попробуй ещё раз")
+
+
+row_main = input_row()
+sum = 0
+for i in range(1, len(row_main), 2):
+    sum += row_main[i]
+print("Сумма нечётных элементов:".upper())
+print("на нечётных позициях элементы", end=" ")
+for j in range(1, len(row_main), 2):
+    print(f"{row_main[j]},", end=" ")
+print(f"ответ: {sum}")
